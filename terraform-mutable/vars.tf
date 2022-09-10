55472bcb05f1205204f7e49c1396670262e676ac
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 13:43:20 +0530

    first push

--- terraform-mutable/vars.tf
+++ terraform-mutable/vars.tf
@@ -3,6 +3,5 @@ variable "COMPONENT" {}
 variable "INSTANCE_TYPE" {}
 variable "SPOT_INSTANCE_COUNT" {}
 variable "OD_INSTANCE_COUNT" {}
-variable "APP_PORT" {}
 variable "WORKSPATION_IP" {}
 variable "APP_VERSION" {}
